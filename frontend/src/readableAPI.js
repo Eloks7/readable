4ff70c5ede927876cd0be6efc692d6397256b0c9
Eloka Ogbonnia <elokakene@gmail.com>
2023-03-17 21:57:13 +0100

    Update readableAPI.js

--- frontend/src/readableAPI.js
+++ frontend/src/readableAPI.js
@@ -1,6 +1,11 @@
-const baseAddress = 'http://localhost:3001';
+const api = 'http://localhost:3001';
 
 const headers = {
     'Accept': 'application/json',
-    'Authorization': token
-  }
+    'Authorization': 'JWT'
+}
+
+export const getCategorries = () => 
+    fetch(`${api}/categories`, { headers })
+        .then(res => res.json())
+        .then(data => data.categories)
