1c069228b18aa4ebad9bc51842d69e3db82c7f5c
Eloka Ogbonnia <elokakene@gmail.com>
2023-03-19 23:31:06 +0100

    Update postSlice.js

---
+++ frontend/src/features/post/postSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    posts: {}
+}
+
+const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        getPosts: (state, action) => {
+            state.posts = action.payload
+        },
+        addPost: (state, action) => {
+            state.posts.push(action.payload)
+        }
+    }
+})
