f5f03159afcf57f659eb4ee63e946961ac24a83b
Eloka Ogbonnia <elokakene@gmail.com>
2023-03-20 20:48:21 +0100

    fetching initial categories

--- frontend/src/features/category/categorySlice.js
+++ frontend/src/features/category/categorySlice.js
@@ -1,18 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { getCategories } from '../../readableAPI';
 
 const initialState = {
     categories: {}
 }
 
+export const fetchCategories = createAsyncThunk('category/fetchCategories', () => {
+    return axios
+        .get(getCategories)
+        .then(res => res.data)
+})
+
 const categorySlice = createSlice({
     name: 'category',
     initialState,
     reducers: {
-        getCategories: (state, action) => {
+        // getCategories: (state, action) => {
+        //     state.categories = action.payload
+        // }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchCategories.pending, (state) => {
+            state.categories = {}
+        })
+        builder.addCase(fetchCategories.fulfilled, (state, action) => {
             state.categories = action.payload
-        }
+        })
+        builder.addCase(fetchCategories.rejected, (state) => {
+            state.categories = {}
+        })
     }
 })
 
 export default categorySlice.reducer;
-export const { getCategories } = categorySlice.actions;
+// export const { fetchCategories } = categorySlice.actions;
