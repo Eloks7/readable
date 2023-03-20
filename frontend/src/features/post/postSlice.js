0d46fa3c68c901a0f59de78337f7c3e6fa6ada32
Eloka Ogbonnia <elokakene@gmail.com>
2023-03-20 22:06:48 +0100

    Update postSlice.js

--- frontend/src/features/post/postSlice.js
+++ frontend/src/features/post/postSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { getPosts } from '../../readableAPI';
+import { getPosts, addPost } from '../../readableAPI';
 
 const initialState = {
     posts: {}
@@ -12,6 +12,12 @@ export const fetchPosts = createAsyncThunk('post/fetchPosts', () => {
         .then(res => res.data)
 })
 
+export const createPost = createAsyncThunk('post/createPost', () => {
+    return axios
+        .post(addPost)
+        .then(res => res.post)
+})
+
 const postSlice = createSlice({
     name: 'post',
     initialState,
@@ -19,9 +25,9 @@ const postSlice = createSlice({
         // getPosts: (state, action) => {
         //     state.posts = action.payload
         // },
-        addPost: (state, action) => {
-            state.posts[action.payload.id] = action.payload
-        }
+        // addPost: (state, action) => {
+        //     state.posts[action.payload.id] = action.payload
+        // }
     },
     extraReducers: (builder) => {
         builder.addCase(fetchPosts.pending, (state) => {
@@ -33,6 +39,11 @@ const postSlice = createSlice({
         builder.addCase(fetchPosts.rejected, (state) => {
             state.posts = {}
         })
+        // builder.addCase(createPost.pending, (state) => {})
+        builder.addCase(createPost.fulfilled, (state, action) => {
+            state.posts[action.payload.id] = action.payload
+        })
+        // builder.addCase(createPost.rejected, (state) => {})
     }
 })
 
