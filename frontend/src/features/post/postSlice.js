1f572556e0b4f0bf633c1c936791ebec8ddeffd0
Eloka Ogbonnia <elokakene@gmail.com>
2023-03-20 21:01:55 +0100

    fetching initial posts

--- frontend/src/features/post/postSlice.js
+++ frontend/src/features/post/postSlice.js
@@ -1,18 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { getPosts } from '../../readableAPI';
 
 const initialState = {
     posts: {}
 }
 
+export const fetchPosts = createAsyncThunk('post/fetchPosts', () => {
+    return axios
+        .get(getPosts)
+        .then(res => res.data)
+})
+
 const postSlice = createSlice({
-    name: 'posts',
+    name: 'post',
     initialState,
     reducers: {
-        getPosts: (state, action) => {
-            state.posts = action.payload
-        },
+        // getPosts: (state, action) => {
+        //     state.posts = action.payload
+        // },
         addPost: (state, action) => {
-            state.posts.id = action.payload
+            state.posts[action.payload.id] = action.payload
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchPosts.pending, (state) => {
+            state.posts = {}
+        })
+        builder.addCase(fetchPosts.fulfilled, (state, action) => {
+            state.posts = action.payload
+        })
+        builder.addCase(fetchPosts.rejected, (state) => {
+            state.posts = {}
+        })
     }
 })
+
+export default postSlice.reducer;
