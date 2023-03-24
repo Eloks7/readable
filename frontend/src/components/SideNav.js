490c8887cf9c8f3a8e09548806392e15eeb3c462
Eloka Ogbonnia <elokakene@gmail.com>
2023-03-24 19:51:46 +0100

    components

---
+++ frontend/src/components/SideNav.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class SideNav extends Component {
+    render() {
+        return(
+            <div className="p-4">
+
+            </div>
+        )
+    }
+}
